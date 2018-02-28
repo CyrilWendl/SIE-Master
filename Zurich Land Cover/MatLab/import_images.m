39f3fc796242c731ed81177ee8b9d3fd71a3f60f
Cyril Wendl <cyrilwendl@gmail.com>
2018-02-28 11:02:56 +0000

    Fully trained UNet with 64*64 batch size

--- Zurich Land Cover/MatLab/import_images.m
+++
@@ -1,7 +0,0 @@
-function im_treated = import_images(im)
-    %im_treated = histeq(im);
-    %im_treated = rgb2gray(im);
-    im_treated = imread(im);
-    %im_treated = imresize(im_treated,[36 36]);
-end
-
--- Zurich Land Cover/MatLab/import_labels.m
+++
@@ -1,7 +0,0 @@
-%% function for getting categorical data from labels
-function data = import_labels(im)
-    data = imread(im)>125;
-    data = data+1;
-    %data = imresize(data,[36 36]);
-    data = categorical(data,[1 2],{'background' 'road'});
-end
